b559faa02c248cbc4c5e4d82e91c3c67cfaf9911
Ghayath Al-Obaidi <37382927+GhayathAl-Obaidi@users.noreply.github.com>
2021-03-06 15:24:51 -0800

    Update ssh-into-ec2.sh

--- ssh-into-ec2.sh
+++ ssh-into-ec2.sh
@@ -1,2 +1,26 @@
-Last login: Sat Mar  6 10:31:48 on console
-(base) Ghayaths-MacBook-Pro:~ g$ this is a test
+# Bash commands used in this repository
+
+# Ensure key is publicly viewable
+chmod 400 week3lab3.pem
+
+# SSH into EC2
+ssh -i "week3lab3.pem" ec2-user@ec2-52-2-88-211.compute-1.amazonaws.com
+
+# Check Python Version
+python --version
+
+# Check AWS CLI version
+aws --version
+
+# Setup AWS Configure
+aws configure
+
+# Call on AWS CLI help menu
+aws help
+
+# Call on AWS s3 CLI help menu
+aws s3 help
+
+# List S3 buckets
+aws s3 ls
+aws s3api list-buckets
